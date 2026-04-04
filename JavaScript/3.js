81704ad61d600909af190ab315f890bf143d3435
Vaibhav Lohar <vaibhavgithub109@gmail.com>
2026-04-04 09:50:58 +0530

    Add files via upload

---
+++ JavaScript/3.js
@@ -0,0 +1,7 @@
+
+
+//  Question
+//  Print the current year using JavaScript's Date object.
+
+let date = new Date().getFullYear();
+console.log(date);
---
+++ JavaScript/4.js
@@ -0,0 +1,9 @@
+
+
+//  Question
+//  Store first name and last name separately and print the fullName.
+
+let fname = 'Vaibhav';
+let lname = 'Lohar';
+
+console.log(`Full Name = ${fname} ${lname}`);
