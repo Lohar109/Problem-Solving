68ded9c8a05cfa2fa97f99730399c9decbc80f2d
Lohar109 <vaibhavgithub109@gmail.com>
2026-04-05 20:25:56 +0530

    solve: sum of two integers

---
+++ sum_of_two_integers.js
@@ -0,0 +1,41 @@
+// Write a program that prints the sum of two numbers.
+
+// Examples 1:
+
+// Sample Input:
+// 10
+// 20
+
+// Expected Output: 30
+
+// Examples 2:
+
+// Sample Input:
+// -10
+// 10
+
+// Expected Output: 0
+
+// Input Format
+
+// Two integers a and b in two separate lines.
+
+// Output Format
+
+// Print a single integer, the sum of a and b. For Javascript, Print the sum of a and b.
+
+// Constraints
+
+// -1000 <a, b<-1000
+
+// The input will always consist of two valid integers.
+
+
+
+
+function sum(a, b) {
+    console.log(a + b);
+
+}
+
+module.exports = {sum}
