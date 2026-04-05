a89656f728a977fd82de2b25fb4322d73c2ed613
Vaibhav Lohar <vaibhavgithub109@gmail.com>
2026-04-05 09:05:51 +0530

    Add files via upload

---
+++ JavaScript/12.js
@@ -0,0 +1,12 @@
+
+
+//    Calculate the sum of numbers from 1 to 10 using a while loop.
+
+let sum = 0;
+let number = 1;
+
+while (number <= 10) {
+  sum += number;
+  number++;
+}
+console.log("The sum of numbers from 1 to 10 is:", sum);
