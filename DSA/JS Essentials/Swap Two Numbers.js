de43dc19c7f038ccb07ed35cb85401c3c3f396c9
Vaibhav Lohar <vaibhavgithub109@gmail.com>
2026-04-06 11:28:39 +0530

    Solve: Swap Two Numbers

---
+++ DSA/JS Essentials/Swap Two Numbers.js
@@ -0,0 +1,54 @@
+// Swap Two Numbers
+
+// Easy
+
+// Fundamental Programming
+
+// Description :
+// Write a program that accepts two integers as input and swaps their values. After swapping, return the new values of the two numbers.
+
+// Examples 1:
+
+// Sample Input:
+// 5
+// 10
+
+// Expected Output:
+// 18
+// 5
+
+// Examples 2:
+
+// Sample Input:
+// 0
+// 0
+
+// Expected Output:
+// 0
+// 0
+
+// Input Format:
+
+// The first line contains an integer a. The second line contains an integer b.
+
+// Output Format:
+
+// Print the two numbers after swapping, each on a new line.. JavaScript: Return an array containing the two numbers after swapping.
+
+// Constraints:
+
+// The input integers will be within the range of -2^31 to 2^31-1.
+// The program should handle both positive and negative integers
+
+
+function swapNumbers(a, b) {
+ 
+    let temp = a;
+    a = b;
+    b = temp;
+
+    return [a, b];
+
+}
+
+module.exports = { swapNumbers };
